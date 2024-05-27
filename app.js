1ac0e96fd5570caf2853bdb6664a4be9ecffa6f2
Erdene-Ochir08 <127087090+Erdene-Ochir08@users.noreply.github.com>
2024-05-27 14:01:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 const text = document.querySelector(".text");
 document.getElementById('sendButton').addEventListener('click', function() {
-    
-    // alert('Email sent successfully!');
-            text.innerText= "Hey!.. Long time no see. Thank you for meeting me all this time. If I've made you uncomfortable for any reason, I am really sorry. Meeting you has been full of excitement and happiness. Recently, I realized that no one else makes me feel the way you do... I just want to see you again and listen to new K-pop releases together and talk about our relationship :)";
-    var templateParams = {
+        var templateParams = {
         to_name: 'Recipient Name',
         from_name: 'Your Name',
         message: 'This is a test message'
@@ -22,8 +19,8 @@ document.getElementById('sendButton').addEventListener('click', function() {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    const message = "Hey!.. Long time no see. Thank you for meeting me all this time. If I've made you uncomfortable for any reason, I am really sorry. Meeting you has been full of excitement and happiness. Recently, I realized that no one else makes me feel the way you do... I really want to see you again and listen to new K-pop releases together and talk about our relationship :)";
-    const speed = 50; // Typing speed in milliseconds
+    const message = "Hey!.. Long time no see. Thank you for meeting me all this time. If I've made you uncomfortable for any reason, I am really sorry. Meeting you has been full of excitement and happiness. Recently, I realized that no one else makes me feel the way you do... I really want to see you again. Let's listen to new K-pop releases together and talk about our relationship :)";
+    const speed = 70; // Typing speed in milliseconds
 
     let index = 0;
 
